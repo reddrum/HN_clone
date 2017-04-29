cd83624db384c7d1ff2ea71d1159b38005fa6ec6
“reddrum” <anton.kranz@me.com>
2017-04-29 18:51:08 +0300

    Added comments

--- Gemfile
+++ Gemfile
@@ -39,6 +39,10 @@ gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 
 gem 'acts_as_votable', '~> 0.10.0'
 
+gem 'simple_form', '~> 3.4'
+
+gem 'record_tag_helper', '~> 1.0'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
 
   resources :submissions do
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
       put "like", to: "submissions#upvote"
       put "dislike", to: "submissions#downvote"
     end
+    resources :comments
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
