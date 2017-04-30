b50b73a6b04bbdee9e67087aa7dbb6bff59d2d53
“reddrum” <anton.kranz@me.com>
2017-04-30 22:40:17 +0300

    Added cached votes to submissions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   devise_for :users
+  root 'submissions#index'
 
   resources :submissions do
     member do
@@ -9,7 +10,4 @@ Rails.application.routes.draw do
     end
     resources :comments
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'submissions#index'
 end
